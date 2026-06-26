d2d593129b8b8a2210e2c62e75480052b114e729
Rusher <rusher.musique@gmail.com>
2026-06-26 20:54:59 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,17 @@ using namespace geode::prelude;
 
 #include <Geode/modify/PlayLayer.hpp>
 #include <fmod.hpp>
+#include <Geode/modify/MenuLayer.hpp>
+
+class $modify(MyMenuLayer, MenuLayer) {
+public:
+    void onRobTop(CCObject* sender) {
+        FLAlertLayer::create("HAPPY BIRTHDAY", "HAPPY BIRTHDAY DANIEL!!! did you really think I’d let this day pass quietly? I know the kind of power you vibe with, the kind that keeps climbing no matter what. So here’s your gift, a profile picture wrapped in that same aura, use it anywhere, you know the rule, EVEN IF THE STAIRS NEVER END, WE KEEP CLIMBING!!!", "ok")->show();
+    }
+};
 
 class $modify(SubaruDeath, PlayLayer) {
     static inline FMOD::Sound* s_returningbydeath = nullptr;
-    static inline CCSprite* s_subarustaire = nullptr;
     
     bool init(GJGameLevel* level, bool useReplays, bool dontCreateObjects) {
         if (!PlayLayer::init(level, useReplays, dontCreateObjects)) return false;
@@ -35,45 +42,11 @@ class $modify(SubaruDeath, PlayLayer) {
                 channel->setVolume(Mod::get()->getSettingValue<float>("volume"));
             }
         }
-
-        displaytheimage();
     }
-
-    void displaytheimage() {
-        if (s_subarustaire) {
-            s_subarustaire->removeFromParentAndCleanup(true);
-            s_subarustaire = nullptr;
-        }
-        auto image = CCSprite::create("subarustaire.png"_spr);
-        if (!image) {
-            log::error("Failed to create sprite for returningbydeath");
-            return;
-        }
-        auto winSize = CCDirector::sharedDirector()->getWinSize();
-        image->setPosition(winSize / 2);
-        image->setScale(2.0f); // Adjust the scale as needed
-        this->addChild(image);
-        s_subarustaire = image;
-        image->runAction(CCSequence::create(
-            CCDelayTime::create(0.2f),
-            CCFadeOut::create(0.1f),
-            CCCallFunc::create(this, callfunc_selector(SubaruDeath::removeImage)),
-            nullptr
-        ));
-    }
-
-    void removeImage() {
-        if (s_subarustaire) {
-            s_subarustaire->removeFromParentAndCleanup(true);
-            s_subarustaire = nullptr;
-        }
-    }
-
     ~SubaruDeath() {
         if (s_returningbydeath) {
             s_returningbydeath->release();
             s_returningbydeath = nullptr;
         }
-        removeImage();
     }
 };    
